d5fff6565e154cd64e15a7566e2f2e54b9adc7dc
bash000000 <m2588953@outlook.com>
2026-04-28 22:38:14 +0800

    Update
    Signed-off-by: bash000000 <m2588953@outlook.com>

--- backend/knowgraph/database/types.py
+++ backend/knowgraph/database/types.py
@@ -1,5 +1,8 @@
 import ast
+from collections import OrderedDict
 
+import orjson
+from age.age import parseAgeValue
 from sqlalchemy import Float, String, cast
 from sqlalchemy.dialects.postgresql.base import ischema_names
 from sqlalchemy.sql.type_api import UserDefinedType
@@ -19,7 +22,7 @@ class BM25Vector(UserDefinedType):
             if isinstance(value, str):
                 return value
             if isinstance(value, dict):
-                sorted_dict = dict(sorted(value.items()))
+                sorted_dict = OrderedDict(sorted(value.items()))
                 return str(sorted_dict)
             return value
 
@@ -45,4 +48,41 @@ class BM25Vector(UserDefinedType):
             return self.op("<&>", return_type=Float)(other)
 
 
+class AGType(UserDefinedType):
+    cache_ok = True
+    _string = String()
+
+    def get_col_spec(self, **kw) -> str:
+        return "AGTYPE"
+
+    def bind_processor(self, dialect):
+        def process(value):
+            if value is None:
+                return None
+            if isinstance(value, str):
+                return value
+            else:
+                return orjson.dumps(value).decode("utf-8")
+
+        return process
+
+    def bind_expression(self, bindvalue):
+        return cast(bindvalue, AGType)
+
+    def result_processor(self, dialect, coltype):
+        def process(value):
+            if value is None:
+                return None
+            if isinstance(value, memoryview):
+                value = value.tobytes().decode("utf-8")
+            if isinstance(value, bytes):
+                value = value.decode("utf-8")
+            if isinstance(value, str):
+                return parseAgeValue(value)
+            return value
+
+        return process
+
+
 ischema_names["bm25vector"] = BM25Vector  # type: ignore
+ischema_names["agtype"] = AGType  # type: ignore
