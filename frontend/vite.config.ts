69cae9dbbd705d3589543894f34aff84286870f9
bash000000 <m2588953@outlook.com>
2026-05-13 16:17:26 +0800

    Update
    Signed-off-by: bash000000 <m2588953@outlook.com>

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,17 @@
 import path from "node:path";
-import react from "@vitejs/plugin-react";
+import babel from "@rolldown/plugin-babel";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [
     react({
       jsxImportSource: "@emotion/react",
+    }),
+
+    babel({
+      plugins: ["@emotion/babel-plugin"],
+      presets: [reactCompilerPreset()],
     }),
   ],
   resolve: {
